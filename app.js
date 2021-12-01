5724fb78f13d1c4dea7d679d524782398e7f0b7a
stavrox90 <inrafilus@gmail.com>
2021-12-01 20:46:13 +0800

    update

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const express = require('express');
-const bodyParser = require('body-parser')
 const app = express();
+const path = require('path')
+require('dotenv').config({ path: path.join(__dirname, '.env') })
 
-
+// import routes
 const index = require('./api/routes/index')
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 const bookRoutes = require('./api/routes/books');
 const taskRoutes = require('./api/routes/tasks');
 
-app.use(express.json())
-app.use(bodyParser.json())
-    .use(bodyParser.urlencoded({
-        extended: true
-    })
-)
+// app settings
+app.use(express.urlencoded({ extended: false })).use(express.json())
+app.set('port', +process.env.PORT ?? 5000)
 
 app.use('/', index);
 app.use('/products', productRoutes);
--- server.js
+++ server.js
@@ -1,33 +1,7 @@
-const path = require('path')
-require('dotenv').config({ path: path.join(__dirname, '.env')})
-const express = require('express');
-
-const PORT = process.env.PORT || 3001;
 const app = require('./app');
+const http = require('http');
 
-// app.get('/', (req, res) => {
-//     res.status(200).json({
-//         message: 'Handling GET request to /index',
-//         data: []
-//     });
-    
-// });
+const server = http.createServer(app);
+const port = app.get('port')
 
-app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
-});
-
-// const http = require('http');
-// require('dotenv').config()
-
-// const port = process.env.PORT || 4000;
-
-// const server = http.createServer(app);
-
-// const test = async (knex) => {
-//     console.log('test', knex)
-// }
-
-// test()
-
-// server.listen(port, console.log("now listening on port " + port));
+server.listen(port, console.log("listening server on port: " + port));
